b4722a86aa1cc76325470be17aedd2c32d97fdaf
Muhamed Malkoč <malkocmuhamed999@gmail.com>
2026-05-03 00:48:45 +0200

    enable cors and file replacement

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'https://smart-document-processing-system-q5n3mvgla.vercel.app'
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
