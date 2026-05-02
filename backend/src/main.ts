dd8c8151ec293b1e7901b036062868ff336750ae
Muhamed Malkoč <malkocmuhamed999@gmail.com>
2026-05-03 01:22:41 +0200

    fix enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: [
       'http://localhost:4200',
-      'https://smart-document-processing-system.vercel.app/'
+      'https://smart-document-processing-system.vercel.app',
     ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
