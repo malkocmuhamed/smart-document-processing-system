e8f16166febb8d516766c07a8ead9cfbe7bb0efd
Muhamed Malkoč <malkocmuhamed999@gmail.com>
2026-05-03 15:28:43 +0200

    api swagger addition

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('Smart Docs API')
+    .setDescription('Document processing system API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   app.enableCors({
     origin: [
       'http://localhost:4200',
@@ -12,6 +22,7 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
